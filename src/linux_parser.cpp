b7c00ba084c5810d59ccb85ef021eedc1cceb5b3
Incomplete <incomplete@aixon.co>
2020-05-03 12:12:55 +0800

    Fix kernel version parsing

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -36,13 +36,13 @@ string LinuxParser::OperatingSystem() {
 
 // DONE: An example of how to read data from the filesystem
 string LinuxParser::Kernel() {
-  string os, kernel;
+  string os, _version, kernel;
   string line;
   std::ifstream stream(kProcDirectory + kVersionFilename);
   if (stream.is_open()) {
     std::getline(stream, line);
     std::istringstream linestream(line);
-    linestream >> os >> kernel;
+    linestream >> os >> _version >> kernel;
   }
   return kernel;
 }
